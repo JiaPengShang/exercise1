4032ee1e1c131bc628f1190f204bc53fc3a585ad
jiaPengShang <2422331213@qq.com>
2024-09-04 15:30:41 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -6,21 +6,38 @@ public class Greeter {
   private Locale locale;
   private String name;
 
+  // Constructor
   public Greeter(String language, String country, String name) {
-    locale = new Locale(language, country);
-    this.name = name;
+    if (language == null || language.isEmpty()) {
+      // Set default language to English if not provided
+      language = "en";
+    }
+    if (country == null || country.isEmpty()) {
+      // Set default country to US if not provided
+      country = "US";
+    }
+    this.locale = new Locale(language, country);
+    this.name = name != null ? name : "Guest"; // Default name to 'Guest' if not provided
   }
 
+  // Say hello method
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting") + ", " + name + "!";
   }
 
   public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
+    try {
+      String language = args.length > 0 ? args[0] : "";
+      String country = args.length > 1 ? args[1] : "";
+      String name = args.length > 2 ? args[2] : "";
+
+      Greeter greeter = new Greeter(language, country, name);
+      System.out.println(greeter.sayHello());
+
+    } catch (Exception e) {
+      System.out.println("Error: Please provide arguments in the following format:");
+      System.out.println("java Greeter <language> <country> <name>");
+    }
   }
 }
